5ac511c327c583ca11e9147df6f933ab53b91694
Ivaylo Radulov <164617687+Duly111@users.noreply.github.com>
2024-08-14 12:35:11 +0300

    Fix comments

--- client/src/api/coments-api.js
+++ client/src/api/coments-api.js
@@ -6,7 +6,7 @@ const create = (recipeId,text) => requester.post(BASE_URL,{recipeId,text});
 
 const getAll = (recipeId) =>{
     const params = new URLSearchParams({
-        where: `gameId="${gameId}"`,
+        where: `recipeId="${recipeId}"`,
         load: `author=_ownerId:users`,
     })
     return requester.get(`${BASE_URL}?${params.toString()}`)
